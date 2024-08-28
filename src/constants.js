d24530caa8fdc461b019a005d8a06f574ac999bd
Himanshu Yadav <shorts.himanshuyadav@gmail.com>
2024-08-28 22:24:21 +0530

    first commit

--- src/constants.js
+++ src/constants.js
@@ -4,13 +4,13 @@ export const dialogueData = {
   pc: `This is my Setup or Workstation. I work mostly in JavaScript.
     I'm building Full Stack Web Apps nowadays using MERN. Anyway regardless of the language, I just enjoy programming.
     Here is my <a href="https://github.com/Himanshuyadav23" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I scored 9.1 GPA with Rank 1 in the University.`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I scored 9.1 GPA with Rank 1 in the University. <a href="https://linktr.ee/Himanshuyadavyt" target="_blank">All My Socials are here</a>!`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube and Netflix, read books annd chill after a busy day.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@harkirat1" target="_blank">Harkirat</a>, <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeTime</a>,`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it. On weekends i prefer working on my bed XD`,
-  resume: `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/1IWbRqV1vpEIjjwdAyUhcLqmSUvRyC2_q/edit?usp=sharing&ouid=108996035386855641990&rtpof=true&sd=true" target="_blank">Check it out?</a>
-  Contact me at Shorts.himanshuyadav@gmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/1IWbRqV1vpEIjjwdAyUhcLqmSUvRyC2_q/edit?usp=sharing&ouid=108996035386855641990&rtpof=true&sd=true" target="_blank">Check it out?</a> <br>
+  Contact me at Shorts.himanshuyadav@gmail.com if you have any interesting job opportunities! I also have my own tech blog, <a href="https://yadavhimanshu.hashnode.dev/" target="_blank">Check it out?</a>`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript. `,
   library: `There are a lot of books on my shelves. I read a lot, started reading back in 2019, i mostly prefer reading self help, a bit of fictional and tech books.`,
   exit: `If you want to exit Himanshu's portfolio, just close the tab.`,
