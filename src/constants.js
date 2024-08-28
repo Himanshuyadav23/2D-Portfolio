d3831e81b1693854292afecd434bd62de3027954
Himanshu Yadav <shorts.himanshuyadav@gmail.com>
2024-08-28 23:01:46 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const dialogueData = {
   pc: `This is my Setup or Workstation. I work mostly in JavaScript.
     I'm building Full Stack Web Apps nowadays using MERN. Anyway regardless of the language, I just enjoy programming.
     Here is my <a href="https://github.com/Himanshuyadav23" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I scored 9.1 GPA with Rank 1 in the University. <a href="https://linktr.ee/Himanshuyadavyt" target="_blank">All My Socials are here</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. I scored 9.1 GPA with Rank 1 in the University.Wanna know more about me <a href="https://linktr.ee/Himanshuyadavyt" target="_blank">All My Socials are here</a>!`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube and Netflix, read books annd chill after a busy day.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@harkirat1" target="_blank">Harkirat</a>, <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeTime</a>,`,
